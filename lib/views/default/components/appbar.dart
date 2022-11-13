a149947ab43534ded89829bc0c24d7e5a387e75f
“wonkdance” <wonkdance@gmail.com>
2022-11-13 22:37:56 +0900

    appbar

--- lib/views/default/components/appbar.dart
+++ lib/views/default/components/appbar.dart
@@ -22,8 +22,21 @@ class _DefaultAppBarState extends State<DefaultAppBar> {
         title: Text(
           '${context.select((AppConfig config) => config.title)} / ${context.select((HomeVM value) => value.home.title)}',
         ),
-        background: const FlutterLogo(),
+        background: Image.network(
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=79',
+          fit: BoxFit.cover,
+        ),
       ),
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.account_circle_outlined),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
